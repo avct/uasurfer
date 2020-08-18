9bbc47e163ebe132a294072c26b6f82000fd3ab8
Melvin Moustaid <melvin@autopilothq.com>
2020-08-18 17:35:58 +1000

    add

--- browser.go
+++ browser.go
@@ -20,7 +20,9 @@ import (
 
 // Retrieve browser name from UA strings
 func (u *UserAgent) evalBrowserID(ua string) bool {
-	if strings.EqualFold(ua, "autopilot") {
+	const googleAntiVirus = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246 Mozilla/5.0"
+	if strings.EqualFold(ua, "autopilot") || strings.EqualFold(ua, googleAntiVirus) {
+
 		u.Browser.ID = BrowserBot
 		return u.maybeBot()
 	}
